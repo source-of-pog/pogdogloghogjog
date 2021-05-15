c9d1d34e691963839f74d72354e226956026585d
source-of-pog <84200844+source-of-pog@users.noreply.github.com>
2021-05-15 12:44:12 -0700

    timerėsã
    bvfd

---
+++ hoggy.py
@@ -0,0 +1,52 @@
+import time
+def r(t):
+  time.sleep(1.5) 
+  print(t)
+r("Daddy:Don't worry Peppa, I'm a bit of an expert!")
+r("Peppa:Don't break the wall Daddy!")
+r("George:*Snorksnork!")
+r("Daddy:Shoot.")
+
+a="(go to 100 for a surprise!)how long da timer:"
+
+h="arjun"
+
+import time
+
+z=int(input(a))
+
+for x in range(1,z+1):
+
+    print(x)
+
+    time.sleep(1)
+
+if z <= 5:
+
+    print("hashtag so little") 
+
+else:
+
+    print("proper timer")
+
+if z >= 100:
+
+    for i in range(1,1):
+
+        time.sleep(0.2)
+
+        print("one hundred")
+
+if z < 3:
+
+  print("ya nubb")
+
+n = "blorg"
+
+print(n)
+
+import time
+
+time.sleep(1)
+
+print(n.replace("blorg", "no blorg"))
